388bb994f2493507f21f01c73c72d2312347df79
Zsofia Horvath <horvathzsofia9105@gmail.com>
2021-11-14 20:03:10 +0000

    Update model fields

--- hogwarts_quiz/views.py
+++ hogwarts_quiz/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
+from rest_framework.renderers import JSONRenderer
+from .models import Answer
+from .models import Question
+from .models import Quiz
+from .models import QuizSerializer
+from .models import Result
+from .models import House
 
 
 def home(request):
-    return render(request, 'hogwarts_quiz/home.html', {})
+    houses = House.objects.all()
+    context = {'houses': houses}
+    return render(request, 'hogwarts_quiz/home.html', context=context)
 
 
 def sorting(request):
