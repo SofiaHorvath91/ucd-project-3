ced78d55c67eff85f9eb34f07224bee560571d0e
Zsofia Horvath <horvathzsofia9105@gmail.com>
2021-11-14 19:05:20 +0000

    Adding models to admin site

--- hogwarts_quiz/admin.py
+++ hogwarts_quiz/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Quiz
+from .models import Result
+from .models import Question
+from .models import Answer
+from .models import House
 
-# Register your models here.
+admin.site.register(Answer)
+admin.site.register(Result)
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(House)
